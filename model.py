b368228f518bae2b0864e0d3507f0863c4aa0920
IZeIke <harit20451@outlook.com>
2016-12-22 19:26:36 +0700

    add picture and make more class

--- model.py
+++ model.py
@@ -1,10 +1,51 @@
+import arcade.key
+
+
 class Stickman:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
     def animate(self, delta):
-        self.x += 5
+        self.x += 0
+
+class Turtle:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def animate(self, delta):
+        self.x += 0
+
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.stickman = Stickman(150, 150)
+        self.turtle = Turtle(150,450)
+        self.status_snail = 0
+        self.status_turtle = 0
+
+    def on_key_press(self, key, key_modifiers):
+        if key == arcade.key.RIGHT and self.status_snail == 0:
+            self.stickman.x += 5
+            self.status=1;
+        elif key == arcade.key.LEFT and self.status_snail == 1:
+            self.stickman.x += 5
+            self.status=0;
+        elif key == arcade.key.D and self.status_turtle == 0:
+            self.turtle.x += 5
+            self.status=1;
+        elif key == arcade.key.A and self.status_turtle == 1:
+            self.turtle.x += 5
+            self.status=0;
+
+
+    def animate(self, delta):
+        self.stickman.animate(delta)
+        self.turtle.animate(delta)
+
+
 
 
 
--- stadium.py
+++ stadium.py
@@ -3,9 +3,22 @@ import arcade
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 600
 
-from model import Stickman
+from model import Stickman,World
+
+class ModelSprite(arcade.Sprite):
+    def __init__(self, *args, **kwargs):
+        self.model = kwargs.pop('model', None)
+
+        super().__init__(*args, **kwargs)
+
+    def sync_with_model(self):
+        if self.model:
+            self.set_position(self.model.x, self.model.y)
 
 
+    def draw(self):
+        self.sync_with_model()
+        super().draw()
 
 class GameWindow(arcade.Window):
     def __init__(self, width, height):
@@ -13,18 +26,22 @@ class GameWindow(arcade.Window):
 
         arcade.set_background_color(arcade.color.WHITE)
 
-        self.stickman = Stickman(100,250)
-        self.stickman_sprite = arcade.Sprite('images/run1.png')
+        self.world = World(width,height);
+        self.stickman_sprite = ModelSprite('images/snail.png',model=self.world.stickman)
+        self.turtle_sprite = ModelSprite('images/turtle.png',model=self.world.turtle)
 
 
     def on_draw(self):
         arcade.start_render()
         self.stickman_sprite.draw()
+        self.turtle_sprite.draw()
 
     def animate(self, delta):
-        stickman = self.stickman
-        stickman.animate(delta)
-        self.stickman_sprite.set_position(stickman.x,stickman.y)
+        self.world.animate(delta)
+       # self.stickman_sprite.set_position(self.world.stickman.x, self.world.stickman.y)
+
+    def on_key_press(self, key, key_modifiers):
+        self.world.on_key_press(key, key_modifiers)
 
 
 if __name__ == '__main__':
